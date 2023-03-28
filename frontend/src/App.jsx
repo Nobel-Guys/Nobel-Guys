16025858eabf83b452dd41175c28e460c69538bc
Ricardo Correia <rmscorreia1@gmail.com>
2023-03-28 10:22:19 +0100

    create nav-bar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import Home from "./pages/Home";
-
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
 import "./App.css";
+import axios from "axios";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  axios
+    .get(
+      "https://api.nobelprize.org/2.1/laureates?_ga=2.80023191.1466721709.1679661688-1905291879.1679661688"
+    )
+    .then((response) => {
+      console.error(response.data);
+    });
+
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div>
+      <NavBar />
     </div>
   );
 }
