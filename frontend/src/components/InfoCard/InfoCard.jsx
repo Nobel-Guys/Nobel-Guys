e3c432262e260660118614d226b0043f6bc4fd9c
Ricardo Correia <rmscorreia1@gmail.com>
2023-04-04 13:12:08 +0100

    add info-card styling

--- frontend/src/components/InfoCard/InfoCard.jsx
+++ frontend/src/components/InfoCard/InfoCard.jsx
@@ -4,13 +4,20 @@ import "./InfoCard.css";
 function InfoCard() {
   return (
     <div className="info-card">
-      <h1>1923</h1>
-      <h3>Albert Einstein</h3>
-      <p>
-        Albert Einstein, widely regarded as one of the greatest physicists of
-        all time, was never awarded the Nobel Prize for his theory of
-        relativity.
-      </p>
+      <div className="card-text">
+        <h1>1923</h1>
+        <h2>Albert Einstein</h2>
+        <p>
+          Albert Einstein, widely regarded as one of the greatest physicists of
+          all time, was never awarded the Nobel Prize for his theory of
+          relativity.
+        </p>
+      </div>
+      <img
+        src="https://s4.static.brasilescola.uol.com.br/be/conteudo/images/2-albert-einstein.jpg"
+        alt="enst"
+        width="30%"
+      />
     </div>
   );
 }
